19e8e21b2b1a42440b88b50926ae1a55cb20001a
konrad <ktalaga90@gmail.com>
2021-04-16 21:00:04 +0100

    added functions to submit event so creating a list item and appending it to ul is now possible

--- js/app.js
+++ js/app.js
@@ -7,6 +7,20 @@ document.addEventListener('DOMContentLoaded', () => {
 const submitForm = function(event) {
     event.preventDefault();
 
+    const first_name = event.target.first_name.value;
+    const surname = event.target.surname.value;
+
+    let weight_class = document.querySelector('#weight_class');
+    weight_class = weight_class.value;
+
+    const fighterListElement = document.createElement('li');
+    fighterListElement.textContent = `Fighter: ${first_name} ${surname} Weight Class: ${weight_class}`;
+
+    const fightersList = document.querySelector('#fighters_list');
+    fightersList.appendChild(fighterListElement);
+
+    
+    
     
 }
 
